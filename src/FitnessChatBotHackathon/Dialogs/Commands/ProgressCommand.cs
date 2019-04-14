ecbce66c718f3e07e736c8fa21870d48d43ffd4d
Czarniecki, Jacek <jacek.czarniecki@ihsmarkit.com>
2019-04-14 04:32:49 +0200

    #3 Fix accidental emoticon `8)`

--- src/FitnessChatBotHackathon/Dialogs/Commands/ProgressCommand.cs
+++ src/FitnessChatBotHackathon/Dialogs/Commands/ProgressCommand.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Fitness.ChatBot.Dialogs.Answer;
 using Fitness.ChatBot.Utils;
@@ -27,13 +29,50 @@ namespace Fitness.ChatBot.Dialogs.Commands
             {
                 await ctx.Context.Senddd("You need to answer questions at least once");
             }
+            else
+            {
+                var allQuestions = answerState.Questions.ToArray();
+                var last3Weeks = allQuestions.Reverse().Take(21)
+                    .Select((x, i) => (stats: x, week: Math.Floor((float) i / 7)))
+                    .GroupBy(x => x.week)
+                    .Select(x => (
+                        week: x.Key,
+                        averageActivity: x.Select(a => a.stats.ActivityScore).Cast<int>().Average() * 5,
+                        averageFood: x.Select(a => a.stats.FoodScore).Cast<int>().Average() * 5,
+                        averageSleep: x.Select(a => a.stats.SleepScore).Cast<int>().Average() * 5
+                    ))
+                    .ToArray()
+                    .Reverse()
+                    .ToArray();
 
-            await ctx.Context.Senddd("TODO: **Progress**");
+                var activityTrend = last3Weeks
+                    .Select((x, i) => (week: i, value: x.averageActivity, change: i == 0 ? "" : ChangeOf(x.averageActivity, last3Weeks[i - 1].averageActivity)))
+                    .ToArray();
+                var foodTrend = last3Weeks
+                    .Select((x, i) => (week: i, value: x.averageActivity, change: i == 0 ? "" : ChangeOf(x.averageActivity, last3Weeks[i - 1].averageActivity)))
+                    .ToArray();
+                var sleepTrend = last3Weeks
+                    .Select((x, i) => (week: i, value: x.averageActivity, change: i == 0 ? "" : ChangeOf(x.averageActivity, last3Weeks[i - 1].averageActivity)))
+                    .ToArray();
+
+                await ctx.Context.Senddd($"These are your results from last {Math.Max(21, allQuestions.Length)} days:");
+                await ctx.Context.Senddd(string.Join("\n", new []
+                    {
+                        $"- Activity Habits [target: 6] \n   - \n{string.Join("\n   - ", activityTrend.Select(x => $"{x.change} week {x.week}: **{x.value:0.0}**"))}",
+                        $"- Food Habits [target: 5] \n   - \n{string.Join("\n   - ", foodTrend.Select(x => $"{x.change} week {x.week}: **{x.value:0.0}**"))}",
+                        $"- Sleep Habits [target: 8] \n   - \n{string.Join("\n   - ", sleepTrend.Select(x => $"{x.change} week {x.week}: **{x.value:0.0}**"))}",
+                    }));
+            }
 
             if (ctx.ActiveDialog != null)
             {
                 await ctx.RepromptDialogAsync();
             }
+        }
+
+        private string ChangeOf(double a, double b)
+        {
+            return a < b ? "🡶" : a > b ? "🡵" : "●";
         }
     }
 }
